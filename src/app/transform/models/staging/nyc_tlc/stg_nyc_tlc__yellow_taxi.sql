b6f53fa362c2272a5f46e4a77913d915f262c89f
Jonathon Kobyluck <jkobyluck@gmail.com>
2025-10-26 13:41:36 -0700

    ops: create basic staging model for nyt tlc
    ref AWS-24

---
+++ src/app/transform/models/staging/nyc_tlc/stg_nyc_tlc__yellow_taxi.sql
@@ -0,0 +1,36 @@
+with
+
+yellow_taxi as (
+    select
+        *
+    from
+        {{ source('nyc_tlc', 'yellow_taxi') }}
+),
+
+final as (
+    select
+        vendorid as vendor_id,
+        lpep_pickup_datetime as pickup_datetime,
+        lpep_dropoff_datetime as dropoff_datetime,
+        store_and_fwd_flag,
+        ratecodeid as rate_code_id,
+        pulocationid as pickup_location_id,
+        dolocationid as dropoff_location_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        improvement_surcharge,
+        total_amount,
+        payment_type,
+        trip_type,
+        congestion_surcharge
+    from
+        yellow_taxi
+)
+
+select * from final

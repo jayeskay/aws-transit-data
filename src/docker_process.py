9783db8e69367fb81462a2f3e2766795930cde88
Jonathon Kobyluck <jkobyluck@gmail.com>
2025-10-26 19:34:31 -0700

    ops: push data to postgres
    close AWS-19

--- src/docker_process.py
+++ src/docker_process.py
@@ -1,16 +1,19 @@
 import os
 import pandas as pd
 from pathlib import Path
+from sqlalchemy import create_engine
 
 USERNAME = os.getenv('POSTGRES_USER')
 PASSWORD = os.getenv('POSTGRES_PASSWORD')
 DATABASE = os.getenv('POSTGRES_DB')
-
+URL      = f'postgresql://{USERNAME}:{PASSWORD}@pgdatabase:5432/{DATABASE}'
 
 def ingest():
     print('Let\'s get started!')
 
-    print(f'postgresql://{USERNAME}:{PASSWORD}@pgdatabase:5432/{DATABASE}')
+    # create postgres engine
+    print(f'{URL=}')
+    engine = create_engine(URL)
 
     # create Path object for the current working directory
     cwd = Path.cwd()
@@ -26,6 +29,11 @@ def ingest():
             # read csv
             df = pd.read_csv(file_path, nrows=100)
             print(df.info())
+
+            # push to table
+            table_name = item.split('.')[0].replace('-','')
+            print(f'{table_name=}')
+            df.to_sql(table_name, engine, if_exists='replace', index=False) 
 
 
 if __name__ == '__main__':
